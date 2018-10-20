75a2267e8675b7a154e969636c3f0450118c895e
socisomer <41864144+socisomer@users.noreply.github.com>
2018-10-20 07:53:10 +0200

    Minor changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='Sallust',
-        version='0.13',
+        version='0.15',
         packages=['Sallust', 'Sallust.GUI', 'Sallust.Tools'],
         url='https://github.com/socisomer/Sallust',
         license='GNU General Public License v3.0',
