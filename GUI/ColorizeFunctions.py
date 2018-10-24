5beef6c51af4d11cde90b24275c227b3b3968930
socisomer <41864144+socisomer@users.noreply.github.com>
2018-10-24 22:26:46 +0200

    Optimized code

--- GUI/ColorizeFunctions.py
+++ GUI/ColorizeFunctions.py
@@ -21,7 +21,7 @@ class ColorizePythonFunctions:
                                 "__getattr__", "__getattribute__", "__setattr__", "__delattr__", "__dir__",
                                 "__class__ ", "__get__", "__set__", "__delete__", "__set_name__", "__slots__",
                                 "__init_subclass__", "__instancecheck__", "__subclasscheck__", "__class_getitem__",
-                                "__call__", "__len__", "__length_hint__","__getitem__", "__getitem__", "__getitem__",
+                                "__call__", "__len__", "__length_hint__", "__getitem__", "__getitem__", "__getitem__",
                                 ]
 
     def update_color(self):
@@ -32,8 +32,8 @@ class ColorizePythonFunctions:
 
         # Colorize the strings
         self.colorize_from_to('"""', '"""', Constants.string_character_color, to_code_included=True)
-        self.colorize_strings('"', '"', Constants.string_character_color, to_code_included=True)
-        self.colorize_strings("'", "'", Constants.string_character_color, to_code_included=True)
+        self.colorize_from_to('"', '"', Constants.string_character_color, to_code_included=True, search_gap="+ 2 char")
+        self.colorize_from_to("'", "'", Constants.string_character_color, to_code_included=True, search_gap="+ 2 char")
         # Colorize the step method name
         self.colorize_from_to("def test", '(', "#e7c51e")
         # Colorize all the key python words
@@ -90,7 +90,7 @@ class ColorizePythonFunctions:
         # Raise the tag over the tag 'function' in order to be shown
         self.text.tag_raise(tag_name, self.tag)
 
-    def colorize_from_to(self, from_code, to_code, color, to_code_included=False):
+    def colorize_from_to(self, from_code, to_code, color, to_code_included=False, search_gap= "+ 1 char"):
         """this function searches for all strings  that starts as 'from_code' and ends as 'to_code'
          inside the given 'Text' widget and changes it's color to 'color' if they are tagged with 'function'
 
@@ -151,62 +151,3 @@ class ColorizePythonFunctions:
         # Raise the tag over the tag 'function' in order to be shown
         self.text.tag_raise(tag_name, self.tag)
 
-    def colorize_strings(self, from_code, to_code, color, to_code_included=False):
-        """this function searches for all strings  that starts as 'from_code' and ends as 'to_code'
-        inside the given 'Text' widget and changes it's color to 'color' if they are tagged with 'function'
-
-        :type from_code: (str) the starting point of the string
-        :type to_code: (str) the end point of the string
-        :type color: (str) the color to be assigned
-        :type to_code_included: (bool) if the end point should be colorized too
-        """
-
-        # Create the tag for the strings of the function
-        tag_name = "color_string"
-
-        # if the tag it's not already created append the tag to all color tags
-        if tag_name not in self.color_tags:
-            self.color_tags.append(tag_name)
-
-        # Configure the tag letter color
-        self.text.tag_configure(tag_name, foreground=color)
-
-        # Get the length of the 'from_code' string
-        from_code_length = "+" + str(len(from_code)) + " char"
-
-        # If to_code_included then calculate the length of 'to_code' in order to be colorized
-        if to_code_included:
-            to_code_length = "+" + str(len(to_code)) + " char"
-        else:
-            to_code_length = ""
-
-        # Initialize the search_index
-        search_index = "1.0"
-        search_index_end = None
-
-        while search_index != "+ 1 char":
-            try:
-                # search the 'from_code' string starting at 'search index', returns the index if there is a coincidence
-                search_index = self.text.search(from_code, search_index, "end", nocase=1)
-                # search the 'to_code' string starting at 'search index' and returns the index if there is a coincidence
-                search_index_end = self.text.search(to_code, search_index + from_code_length, "end", nocase=1)
-
-                # Get the tags that are assigned to the start index and end index
-                tag_names = self.text.tag_names(search_index)
-                tag_names_end = self.text.tag_names(search_index_end)
-
-                # if 'function' tag is in both index then colorize all text from 'from_code' to 'to_code'
-                if "function" in tag_names_end:
-                    if "function" in tag_names:
-                        self.text.tag_add(tag_name, search_index, search_index_end + to_code_length)
-
-                # set the search_index to the current coincidence
-                search_index = search_index_end + "+ 2 char"
-            except Exception:
-                pass
-            # set the search_index to the current coincidence plus 1 character
-
-            search_index = search_index + "+ 1 char"
-
-        # Raise the tag over the tag 'function' in order to be shown
-        self.text.tag_raise(tag_name, self.tag)
