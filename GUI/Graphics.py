7b0f50ce4a3a90d7cab808ed6f961ad1d767a955
socisomer <41864144+socisomer@users.noreply.github.com>
2018-10-25 05:48:23 +0200

    Optimize code

--- GUI/Graphics.py
+++ GUI/Graphics.py
@@ -94,7 +94,6 @@ class Graphics(tk.Frame):
         self.draw()
         self.table_frame.update_table()
         self.steps_frame.update_table()
-        self.figure_pie.savefig("temp/pie.png")
 
     def draw(self):
 
@@ -126,7 +125,7 @@ class Graphics(tk.Frame):
 
         # Create the Pie Plot
         self.plot = self.figure_pie.add_subplot(111)
-        self.plot.set_title("Results of the test run")
+        self.plot.set_title("Results of the TestCase")
         sizes = [self.controller.test_passed, self.controller.test_failed]
         self.plot.pie(sizes,
                       labels=self.labels,
